c1b62dded24fb8217e8f48d278e62ed753e728c5
Ahmad Faris <isfawwaz@gmail.com>
2022-11-11 16:56:55 +0700

    feat(converter): add utils converter

---
+++ src/converters/utils.js
@@ -0,0 +1,37 @@
+const TWResolveConfig = require('tailwindcss/resolveConfig')
+
+function indentWith (value, size) {
+  return ' '.repeat(size) + value
+}
+module.exports.indentWith = indentWith
+
+/**
+ * Resolves a config.
+ * If passed a string, imports it first.
+ * @param {String | Object} config
+ * @return {Object}
+ */
+function resolveConfig (config) {
+  if (typeof config === 'string') {
+    config = require(config)
+  }
+  return TWResolveConfig(config)
+}
+module.exports.resolveConfig = resolveConfig
+
+function isObject (value) {
+  return !Array.isArray(value) && typeof value === 'object'
+}
+module.exports.isObject = isObject
+
+function sanitizeKey (text) {
+  // console.log({ text });
+  return text.replace(/%/g, '').replace(/, /g, '-')
+}
+module.exports.sanitizeKey = sanitizeKey
+
+function camelToString (key, separator = '-') {
+  const result = key.replace(/([A-Z])/g, ' $1')
+  return result.split(' ').join(separator).toLowerCase()
+}
+module.exports.camelToString = camelToString
