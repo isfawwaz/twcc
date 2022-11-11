9d5ce244883efb6b7ea07e7893595df0273cc01c
Ahmad Faris <isfawwaz@gmail.com>
2022-11-11 22:29:06 +0700

    feat(scss): add scss converter

---
+++ src/converters/scss.js
@@ -0,0 +1,26 @@
+const Converter = require('./converter');
+
+class ScssConverter extends Converter {
+  format = 'scss';
+  prefixContent = '';
+  suffixContent = '';
+
+  _buildVar(name, value) {
+    return `$${name}: ${value};\n`;
+  }
+
+  _isPropertyConvertToString() {
+    return true;
+  }
+
+  _convertObjectToMap(prop, data) {
+    return this._convertObjectToVar(prop, data);
+  }
+
+  _sanitizePropValue(value) {
+    if (Array.isArray(value)) return value.join(', ');
+    return value;
+  }
+}
+
+module.exports = ScssConverter;
